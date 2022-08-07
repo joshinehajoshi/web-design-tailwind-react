c894eeb108d986511001fc50c36bdae7f25879a8
neha-butterpaper <neha.joshi@butterpaper.tech>
2022-08-07 13:46:15 +0530

    typescript

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './components/Home';
+import CardComp from './components/CardComp';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
+      <CardComp />
     </div>
   );
 }
